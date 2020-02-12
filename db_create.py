1d208d883884ed7f8c1305ec0987162609d61481
nsi319 <sairam.nareniyer@gmail.com>
2020-02-12 17:34:45 +0530

    create_db updated

--- db_create.py
+++ db_create.py
@@ -9,12 +9,12 @@ import psycopg2
 # nltk.download('words')
 # nltk.download('wordnet')
 conn = psycopg2.connect(host="ec2-35-172-85-250.compute-1.amazonaws.com",database="dag5m77tnenl1m", user="wyxfedkzbibfmg", password="40d812b02a4b9bfca7047b68bb2893cbbd3ca88496d90e6945909b145166ede9")
-
+cur = conn.cursor()
 print("Opened database successfully")
 #conn.execute('DROP TABLE candidate')
-conn.execute('CREATE TABLE candidate (phone TEXT,email TEXT,linkedin TEXT,exp_years TEXT, duration TEXT,summary TEXT,skills TEXT,experience TEXT,education TEXT,extra TEXT,awards TEXT,filename TEXT, complete_resume TEXT, resume STRING(500) PRIMARY KEY)')
+cur.execute('CREATE TABLE candidate (phone TEXT,email TEXT,linkedin TEXT,exp_years TEXT, duration TEXT,summary TEXT,skills TEXT,experience TEXT,education TEXT,extra TEXT,awards TEXT,filename TEXT, complete_resume TEXT, resume STRING(500) PRIMARY KEY)')
 print("Table created successfully")
-
+cur.close()
 dire = 'assets'
 if os.path.exists(os.path.abspath(dire)):
     shutil.rmtree(dire)
