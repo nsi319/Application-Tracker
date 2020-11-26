8af51465f57a6b65cbdb6f02107e3d5a83aac790
nsi319 <sairam.nareniyer@gmail.com>
2020-11-26 12:17:53 +0530

    Update: Working

---
+++ candidate_table.py
@@ -0,0 +1,14 @@
+import sqlite3
+from models import Candidate
+import nltk
+import shutil,os
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('maxent_ne_chunker')
+# nltk.download('words')
+# nltk.download('wordnet')
+conn = sqlite3.connect('ats.db')
+print("Opened database successfully")
+#conn.execute('DROP TABLE candidate')
+conn.execute('CREATE TABLE resume (phone TEXT,email TEXT,linkedin TEXT,exp_years TEXT, duration TEXT,summary TEXT,skills TEXT,experience TEXT,education TEXT,extra TEXT,awards,filename TEXT, complete_resume TEXT, resume STRING(500) PRIMARY KEY)')
+print("Table created successfully")
