1d59f2d70601e267de87123d1cde8750c029da78
nsi319 <sairam.nareniyer@gmail.com>
2020-12-26 00:16:44 +0530

    Update: Cleared db, Created folders

--- utils/tables/create.py
+++ utils/tables/create.py
@@ -11,26 +11,36 @@ import shutil,os
 # nltk.download('wordnet')
 # open('../../Candidate.db','a').close()
 
-conn = sqlite3.connect('/app/Candidate.db')
+print("Before: ")
+try:
+    cand = Candidate.query.all()
+    print("select all cands")
+    print(cand)
+except:
+    print("No such table")
+
+conn = sqlite3.connect('../../Candidate.db')
 print("Opened database successfully")
 conn.execute('DROP TABLE if exists candidate')
 conn.execute('CREATE TABLE candidate (phone TEXT,email TEXT,linkedin TEXT,exp_years TEXT, duration TEXT,summary TEXT,skills TEXT,experience TEXT,education TEXT,extra TEXT,awards,filename TEXT, complete_resume TEXT, resume STRING(500) PRIMARY KEY)')
 print("Table created successfully")
 
-dire = '/app/assets'
+dire = '../../assets'
 if os.path.exists(os.path.abspath(dire)):
     shutil.rmtree(dire)
     print("Yes assets existed")
 os.makedirs(os.path.abspath(dire))
 
-dire = '/app/all_resumes'
+dire = '../../all_resumes'
 if os.path.exists(os.path.abspath(dire)):
     shutil.rmtree(dire)
     print("Yes all_resumes existed")
 
 os.makedirs(os.path.abspath(dire))
 
+print("After: ")
 cand = Candidate.query.all()
+print("select all cands")
 print(cand)
 
 
